b05a3b8f305caa1f3bc979e5f1fcaeab017f2594
Reginaldo Lopes <reginaldo.lopes.dev@gmail.com>
2023-05-15 15:39:48 -0300

    first attempt to resolve the challenge

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -22,6 +23,19 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+app.get('/api/whoami', function (req, res) {
+  const ip = req.ip;
+
+  const regexLanguage = /^[a-z]{2}-[A-Z]{2}(,[a-z]{2}(;q=\d(\.\d)?)?)*(?=,|$)/;
+
+  const acceptLanguage = req.headers['accept-language'] || "pt-BR,pt;q=0.9";
+  const match = acceptLanguage.match(regexLanguage);
+  const language = match[0];
+
+  const software = req.headers['user-agent'];
+
+  res.json({"ipAddres": ip, "language": language, "software": software});
 });
 
 // listen for requests :)
